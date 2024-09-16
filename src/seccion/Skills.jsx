2b494ffd27c8cb48dcdd38a2e0f1bd85e8e5146c
Fabricio Porras Morera <faporras05@hotmail.com>
2024-09-16 12:33:56 -0600

    Update Skills.jsx

--- src/seccion/Skills.jsx
+++ src/seccion/Skills.jsx
@@ -5,7 +5,7 @@ function Skills() {
     return (
         <section id="skills" className="flex mt-16 flex-col font-semibold fade-in-up px-4 gap-5 lg:px-0">
             <h2 className="dark:text-white text-secondary-dark-theme-color flex items-center gap-x-2 text-4xl"><GiSkills /> Skills</h2>
-            <div className="flex mt-5 center gap-y-11 gap-x-14 sm:gap-x-24 flex-wrap">
+            <div className="grid mt-5 gap-y-11 gap-x-7 sm:gap-x-14 lg:gap-x-20 grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6">
                 {SKILLS.map((item, index) =>
                     <SkillsComponent key={index} SvgIcon={item[0]} title={item[1]} colorHover={item[2]} />
                 )}
